2ddf3e1105e34ced503c1b6ef935613d3dc2e3f8
yuvrajgrover6 <yuvrajgrover6@gmail.com>
2022-09-14 10:54:59 +0530

    .

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -25,39 +25,41 @@ class SignIn extends StatelessWidget {
       ),
       body: Container(
           padding: EdgeInsets.symmetric(horizontal: width * 0.04),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                  width: width,
-                  child: Image.asset(
-                    'assets/images/logo-png.png',
-                    width: width * 0.5,
-                    height: height * 0.3,
-                  )),
-              Text('Email', style: TextStyle(fontWeight: FontWeight.bold)),
-              SizedBox(height: height * 0.01),
-              CustomTextField(hint: 'Enter Email'),
-              SizedBox(height: height * 0.03),
-              Text('Password', style: TextStyle(fontWeight: FontWeight.bold)),
-              SizedBox(height: height * 0.01),
-              CustomTextField(hint: 'Enter Password'),
-              SizedBox(height: height * 0.05),
-              Center(
-                child: Container(
-                    width: width * 0.5,
-                    height: height * 0.06,
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Color(0xff11586b))),
-                        onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomePage())),
-                        child: Text("Sign-In"))),
-              )
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    width: width,
+                    child: Image.asset(
+                      'assets/images/logo-png.png',
+                      width: width * 0.5,
+                      height: height * 0.3,
+                    )),
+                Text('Email', style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: height * 0.01),
+                CustomTextField(hint: 'Enter Email'),
+                SizedBox(height: height * 0.03),
+                Text('Password', style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: height * 0.01),
+                CustomTextField(hint: 'Enter Password'),
+                SizedBox(height: height * 0.05),
+                Center(
+                  child: Container(
+                      width: width * 0.5,
+                      height: height * 0.06,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Color(0xff11586b))),
+                          onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage())),
+                          child: Text("Sign-In"))),
+                )
+              ],
+            ),
           )),
     );
   }
